7e54137ba66bf399587966f4e3a22a1cea4abc10
jaderado <jaderado@yahoo.com>
2016-03-07 20:22:19 -0500

    Committing CreateFlashcards.java

---
+++ app/src/main/java/com/example/arturo/flashcardsparse/CreateFlashcards.java
@@ -0,0 +1,23 @@
+package com.example.arturo.flashcardsparse;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+/**
+ * Created by Owner on 3/7/2016.
+ */
+public class CreateFlashcards extends AppCompatActivity {
+
+TextView CreateFlashcards;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_flashcards);
+
+
+
+
+
+
+    }
+}
