4e55dddc4f5d613a339b595dca31e8674f47eef4
arturoaram <arturoaram0@gmail.com>
2016-04-01 22:16:33 -0400

    Connected the FlashCards with the sets on the Card Layout!

--- app/src/main/java/com/example/arturo/flashcardsparse/UsersFlashcardSets.java
+++ app/src/main/java/com/example/arturo/flashcardsparse/UsersFlashcardSets.java
@@ -1,10 +1,11 @@
 package com.example.arturo.flashcardsparse;
 
-import android.app.Activity;
 import android.app.ProgressDialog;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -22,19 +23,20 @@ import java.util.List;
 /**
  * Created by Owner on 3/28/2016.
  */
-public class UsersFlashcardSets extends Activity {
+public class UsersFlashcardSets extends AppCompatActivity {
     ListView listview;
     ArrayList<FlashCardsSet> arFlashCardsSet;
     ProgressDialog mProgressDialog;
     FlashCardSetAdapter adapter;
     private List<SetsFC> setsList = null;
     ParseUser user;
-    List<FlashCardsSet> lFC;
+    List<ParseObject> fcSets;
     //Context context= getApplicationContext();
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.users_fcsets);
+        setTitle(user.getCurrentUser().getString("Name")+"'s Flashcard Sets");
 
         listview = (ListView) findViewById(R.id.listView);
 
@@ -46,6 +48,7 @@ public class UsersFlashcardSets extends Activity {
         query.findInBackground(new FindCallback<ParseObject>() {
             public void done(List<ParseObject> scoreList, ParseException e) {
                 if (e == null) {
+                    fcSets = scoreList;
                     Log.d("score", "Retrieved " + scoreList.size() + " scores");
                     //arFlashCardsSet.addAll(scoreList);
                     arFlashCardsSet = new ArrayList<FlashCardsSet>();
@@ -63,8 +66,36 @@ public class UsersFlashcardSets extends Activity {
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent intent = new Intent(getApplicationContext(),StartStudyingFC.class);
-                startActivity(intent);
+                //Intent intent = new Intent(getApplicationContext(),StartStudyingFC.class);
+                //startActivity(intent);
+
+                final ParseObject fcSetParse = fcSets.get(i);
+                FlashCardsSet fcSet = arFlashCardsSet.get(i);
+
+                ParseQuery<ParseObject> query = ParseQuery.getQuery("FlashCard");
+                query.whereEqualTo("Parent", fcSetParse);
+                query.findInBackground(new FindCallback<ParseObject>() {
+                    public void done(List<ParseObject> FlashCardsList, ParseException e) {
+                        if (e == null) {
+                            Log.d("MY FLASHCARDSSSSSSSS", "Retrieved " + FlashCardsList.size() + " scores");
+                            setContentView(R.layout.display);
+                            setTitle(fcSetParse.getString("Title"));
+                            RecyclerView recList = (RecyclerView) findViewById(R.id.cardList);
+                            LinearLayoutManager llm = new LinearLayoutManager(getApplicationContext());
+                            llm.setOrientation(LinearLayoutManager.VERTICAL);
+                            recList.setLayoutManager(llm);
+
+                            ListViewAdapter cs = new ListViewAdapter(createList(FlashCardsList.size(),FlashCardsList));
+                            recList.setAdapter(cs);
+
+
+                        } else {
+                            Log.d("score", "Error: " + e.getMessage());
+                        }
+                    }
+                });
+
+
             }
         });
 
@@ -150,6 +181,7 @@ public class UsersFlashcardSets extends Activity {
             //  });
         }
     }
+
     public List<FlashCardsSet> parseObjectToFlashCardSetConverter(List<ParseObject> list){
         List<FlashCardsSet> lFC = new ArrayList<FlashCardsSet>();
         FlashCardsSet fcObject;
@@ -162,6 +194,17 @@ public class UsersFlashcardSets extends Activity {
         }
         }
         return lFC;
+    }
+
+    private List<FlashCard> createList(int size, List<ParseObject> fcList){
+        List<FlashCard> result=new ArrayList<FlashCard>();
+        size = size-1;
+        for(int i=0;i<=size;i++){
+
+            FlashCard co=new FlashCard(fcList.get(i).get("term").toString(),fcList.get(i).get("description").toString());
+            result.add(co);
+        }
+        return result;
     }
 }
 
