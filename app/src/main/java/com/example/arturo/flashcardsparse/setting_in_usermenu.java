eef5c242bebd96d3bdaa934edff88ac26cd2ccd7
jaderado <jaderado@yahoo.com>
2016-04-08 17:04:18 -0400

    settings code fixed-logoutButton, savebtn, donotdisturb button

--- app/src/main/java/com/example/arturo/flashcardsparse/setting_in_usermenu.java
+++ app/src/main/java/com/example/arturo/flashcardsparse/setting_in_usermenu.java
@@ -7,10 +7,13 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
+import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.RequestPasswordResetCallback;
@@ -26,23 +29,47 @@ public class setting_in_usermenu extends AppCompatActivity {
     EditText newPwd;
     EditText oldPwd;
     ImageButton doNotdisturb;
+    ParseUser userP=ParseUser.getCurrentUser();
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
 
-        newPwd=(EditText)findViewById(R.id.newpwd);
-        oldPwd=(EditText)findViewById(R.id.oldpwd);
+        newPwd=(EditText)findViewById(R.id.enternewpwd);
+        oldPwd=(EditText)findViewById(R.id.enteroldpwd);
         Logout=(Button)findViewById(R.id.logOutButton);
         saveButton=(Button)findViewById(R.id.savePwdbtn);
         doNotdisturb =(ImageButton)findViewById(R.id.doNotdistubBTN);
         setTitle("Settings");
 
 
+//hides the status bar
+        //not sure what setContentView does
+doNotdisturb.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+      //  setContentView(R.layout.studying_cards);
+    }
+});
 
 
+      saveButton.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              if(oldPwd.getText().toString().equals(userP.getString("Password"))){
+                  userP.setPassword(newPwd.getText().toString());
+              }
 
+          }
+      });
 
-
+Logout.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        ParseUser.logOut();
+    }
+});
 }
 
 
